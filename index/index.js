1f4d8a49ea7c664dab3719f0eca7598261ee0c74
Michael Redruello <redruellomichael@gmail.com>
2021-09-24 14:56:42 +0200

    Homeworks

---
+++ index/index.js
@@ -0,0 +1,5 @@
+console.log("The page has been loaded, and this message came from index.js")
+
+function buttonClicked() {
+    document.querySelector("h1").innerText = "The button has been clicked!"
+}
