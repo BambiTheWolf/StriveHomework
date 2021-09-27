d83c1f519d6f54a585564ec44503310646a6b4bd
Michael Redruello <redruellomichael@gmail.com>
2021-09-27 12:15:02 +0200

    Basics of JS upload

--- Module 1/Day6/M1D6.js
+++ Module 1/Day6/M1D6.js
@@ -15,34 +15,55 @@ ASSIGNMENT RULES
    Create a variable called test and assign a string value to it.
 */
 
+let test = "Test text";
+
 /* EXERCISE B
     Create a variable called sum and assign to it the result of the sum between the numbers 10 and 20.
 */
+
+let sum = 10 + 20;
 
 /* EXERCISE C
     Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
 
+let random = Math.floor(Math.random() * 21);
+
 /* EXERCISE D
     Create a variable called me and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
 */
+
+const me = {
+    name: "Michael Marco",
+    surname: "Redruello",
+    age : 22,
+};
 
 /* EXERCISE E
     Write a piece of code for programmatically removing the age property from the previously create object.
 */
 
+delete me.age;
+
 /* EXERCISE F
    Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
 */
+
+me.skills = ["JavaScript", "Python"];
 
 /* EXERCISE G
    Write a piece of code for programmatically removing the last skill from the skills array inside the me object.
 */
 
+me.skills.pop();
+
 // JS Functions
 /* EXERCISE 1
     Write a function called dice; it should randomize an integer number between 1 and 6.
 */
+
+const dice = function()
+
 
 /* EXERCISE 2
     Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
