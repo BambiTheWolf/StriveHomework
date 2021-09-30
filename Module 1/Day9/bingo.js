4b3c7386309e62ca0cf3ec236a661d02286d32ce
Michael Redruello <redruellomichael@gmail.com>
2021-09-30 16:32:07 +0200

    Update

--- Module 1/Day9/bingo.js
+++ Module 1/Day9/bingo.js
@@ -5,7 +5,7 @@ const bingoBoard = function() {
 
     const cellsContainerNode = document.getElementById("cells-container")
 
-    for (let cellsNumber = 1; cellsNumber < 76; cellsNumber++) {
+    for (let cellsNumber = 1; cellsNumber < 77; cellsNumber++) {
 
         let newCellsNode = document.createElement("div")
         newCellsNode.classList.add("cell")
@@ -17,8 +17,39 @@ const bingoBoard = function() {
     }
 }
 
-const = selectCells = function(eventData) {
-    const current
+const selectCells = function(eventData) {
+    const currentSelectedCells = getSelectedCells()
+    if (currentSelectedCells != null) {
+        currentSelectedCells.classList.remove("selected")
+    }
+    const clickedCellsNumber = eventData.target
+
+    clickedCellsNumber.classList.add("selected")
+}
+
+const getSelectedCells = function() {
+    return document.querySelector(".selected")
+}
+
+const saveToDisk = function() {
+
+    let json = JSON.stringify(bingoCells)
+
+    // Save our serialized JSON string to the local storage
+    localStorage.setItem("strive-bingo-data", json)
+}
+
+const readFromDisk = function() {
+
+    // Read saved data from the local storage
+    let json = localStorage.getItem("strive-bingo-data")
+
+    if (json === null) {
+        bingoCells = { }
+    }
+    else {
+        bingoCells = JSON.parse(json)
+    }
 }
 
 window.onload = function() {
