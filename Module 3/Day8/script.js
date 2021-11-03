c42d15bc3588a4d5ab9d32cad32db877bb5d7122
Michael Redruello <redruellomichael@gmail.com>
2021-11-03 17:04:48 +0100

    update

--- Module 3/Day8/script.js
+++ Module 3/Day8/script.js
@@ -1,17 +1,14 @@
-
-window.onload = () => {
-    shopData()
-}
-
 const shopData = async () => {
-    let response = await fetch('https://striveschool-api.herokuapp.com/api/product/', {
+    try {
+    const response = await fetch('https://striveschool-api.herokuapp.com/api/product/', {
         headers: {
             "Content-Type": "application/json",
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyYTYyZWFhY2FhMjAwMTU1MmE1OGUiLCJpYXQiOjE2MzU5NTIxNzQsImV4cCI6MTYzNzE2MTc3NH0.r6UjMr4j4TPX6MjOXPj-pP_7YwXBiQtSDlt1HroH9aw"
         }
     })
+    console.log(response)
 
-    const body = await response.json()
-    console.log(body)
-
+    if (!response.ok) {
+        throw "Something went wrong"
+    }
 }
