7c6d02f191f7b0252a8c3083e3a8f184f266153c
Michael Redruello <redruellomichael@gmail.com>
2021-11-09 16:56:24 +0100

    Jumbotron (work in progress)

--- Module4/Day2/strivebooks-app/src/App.js
+++ Module4/Day2/strivebooks-app/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavbar from './components/MyNavbar';
 import MyFooter from './components/MyFooter';
+import { Jumbotron } from 'react-bootstrap';
 
 
 function App() {
   return (
     <div>
       <MyNavbar brand= "StriveBooks" />
-      
+      <Jumbotron />
     </div>
   );
 }
---
+++ Module4/Day2/strivebooks-app/src/components/Welcome.jsx
@@ -0,0 +1,20 @@
+import { Jumbotron, Container, Button } from "react-bootstrap";
+
+import React from "react";
+
+const Jumbotron = () => (
+    <Jumbotron>
+      <h1>Regular, Jumbotron!</h1>
+      <p>
+        This is a simple Jumbotron example.
+      </p>
+  
+      <p>
+        <Button variant="primary">
+          Primary Button
+        </Button>
+      </p>
+    </Jumbotron>
+)
+
+export default Jumbotron
