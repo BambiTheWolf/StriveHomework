b2d98f2bd4c1656e2f1249a12a8d6fe595c38a2e
Michael Redruello <redruellomichael@gmail.com>
2021-11-09 16:05:01 +0100

    Strivebooks setup

---
+++ Module4/Day2/strivebooks-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavbar from './components/MyNavbar';
+
+
+function App() {
+  return (
+    <div>
+      <MyNavbar brand= "StriveBooks" />
+    </div>
+  );
+}
+
+export default App;
